944b662e97156e751c2fb47011cfd85f47e43d2c
p2405427 <p2405427@mpu.edu.mo>
2026-03-27 11:27:46 +0800

    Update level.js

--- js/level.js
+++ js/level.js
@@ -3,16 +3,16 @@
 character: 角色
 interact_area: 可交互區域
 interact_item: 可拾取物件
+
+加入以下類別：
+data-floor = 0, 1, 2
 */
 //
-constant mainCharacterRec = main.getboundingclientrect();
-
-
 
 // clike the room
 $(".room").on("click", function() {
-  if($(".selcetedGuest")){
-    GuestToRoom($(this))
+  if($(".selcetedGuest").length){
+    guestToRoom($(this))
   }else{
     moveTo($(this))
   }
@@ -20,36 +20,50 @@ $(".room").on("click", function() {
 
 // clike the interact_item
 $(".interact_item").on("click", function(){
-  sameFloor(mainCharacter, $(this))
+  sameFloor($("mainCharacter"), $(this))
   moveTo($(this))
   trolleyOP($(this))
 })
 
-function GuestToRoom(room){
-  if(not room.attr("active")){
-  sameFloor($(".selcetedGuest"), room)
-  $(".selcetedGuest").x = (room.right-room.left)/2+"px"
-  $(".selcetedGuest").y = room.bottom + "px"
+function guestToRoom($room){
+  if(!$room.data("active")){
+  let $guest = $("selectedGuest")
+  sameFloor($guest, $room)
+  let roomOff = room.Offset()
+  $guest.css{
+    left: (roomOff.left + $room.outerWidth/2) + "px"
+    top: (roomOff.top + $room.outerHeight/2) + "px"
+    } 
 }
 
-function moveTo(character = maincharacter, area){
-  if (area is $(".interact_area")){
-    character.x = (area.right-area.left)/2+"px"
-    character.y = area.bottom + "px"
-  }elif(area is $(".interact_item")){
-    ////////////////////////////////////////////
-    character.x = (area.right-area.left)/2+"px"
-    character.y = (area.bottom-area.top)/2+"px"
-  }
+function moveTo(($area, $character = $("#maincharacter)){
+  let areaOff = $area.offset()
+  let left, top           
   
+  if (area.hasClass("interact_area")){
+    left = areaOff.left + $area.outerWidth/2 - $character.outerWidth()/2
+    top = areaOff.top + $area.outerHeight - $character.outerHeight() // same bottom
+  }else if(area.hasClass("interact_item")){
+    left = areaOff.left + $area.outerWidth/2 - $character.outerWidth()/2
+    top = areaOff.top + $area.outerHeight/2 - $character.outerHeight()/2
+  }  
+
+  $character.css({
+    left: left + "px"
+    top: top + "px"
+    transition: 'left 0.2s, top 0.2s'//////////
+  })
 }
 
 function sameFloor(character, elme){
-  FC = character.attr(floor)// 當前樓層，0，1，2
-  FE = elme.attr(floor)
-  if (FC != FE){
-    moveTo(character, $(`left`+ FC))
-    moveTo(character, $(`left`+ FE))
+  let FC = character.data("floor")
+  let FE = elme.data("floor")
+  if (FC !== FE){
+    moveTo($(`#left_${FC}`), $character)
+    setTimeout(()=>{
+      moveTo($(`#left_${FE}`), $character)  
+    },200)
+    
   }
 }
 
