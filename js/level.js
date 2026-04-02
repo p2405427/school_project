19e430f9d1575591314130c51d4a4045306b1cb4
p2405427 <p2405427@mpu.edu.mo>
2026-04-02 19:00:02 +0800

    Add files via upload

--- js/level.js
+++ js/level.js
@@ -8,61 +8,212 @@ interact_item: 可拾取物件
 data-floor = 0, 1, 2
 */
 //
-const speed = 400
-console.log("access level.js!")
-console.log(`speed: ${speed}`)
-$("#mainCharacterandTrolley").css({
-    "left":"300px",
-    "bottom":"250px"
+const speed = 200
+const Cover = $("#cover")
+const coverContent = $("#coverContent")
+let isMoving = false
+
+let guestInterval = null
+let guestCount = 0
+const MAX_GUESTS = parseInt($("#guestMax").text())
+let Count_guest = $("#guestCount")
+console.log(Count_guest)//0
+
+
+class Guest{
+  constructor(id){
+    this.id = id
+  }
+}
+const MainCharacter = $("#mainCharacter")
+
+$(document).ready(async function() {
+    MainCharacter.css({
+        "left": "10%",
+        "bottom": "15%",
+        "transition": "none"
+    })
+    const currentPos = MainCharacter.offset();
+    MainCharacter.css({
+        "position": "absolute",
+        "left": currentPos.left + "px",
+        "top": currentPos.top + "px",
+        "z-index": 99 
+    });
+    
+    console.log("access level.js!")
+    console.log(`speed: ${speed}`)
+
+    await openingAnimation()
+    console.log("..........")
+    startGuestSpawner()
+
 })
+
+function openingAnimation() {
+    return new Promise((resolve) => {
+    let time = 3 
+
+    const timer = setInterval(() => {
+      coverContent.text(time <= 0 ? "GO!" : time)
+
+      coverContent.removeClass("animate-pulse")
+      void coverContent[0].offsetWidth 
+      coverContent.addClass("animate-pulse")
+
+      if (time < 0) {
+        clearInterval(timer);
+        Cover.fadeOut(speed, () => {
+          resolve(); 
+        });
+      }
+
+        time--
+    }, 1000)
+  })
+}
+
+
+function startGuestSpawner() {
+  setTimeout(() => {
+    spawnGuest()
+    
+    guestInterval = setInterval(() => {
+      if (guestCount < MAX_GUESTS) {
+        spawnGuest()
+      } else {
+        clearInterval(guestInterval)
+      }
+    }, 16000)
+  }, 3000)
+}
+
+function spawnGuest() {
+  if (guestCount >= MAX_GUESTS) return
+  
+  const guestId = `guest_${Date.now()}` 
+  new Guest(guestId)
+  const $guest = $(`
+    <div id="${guestId}" class="guest interact_area" data-floor="0">🧑</div>
+  `)
+  
+  $("#waitting").append($guest)
+  
+  $guest.on("click", function(e) {
+    e.stopPropagation()
+    console.log(`click ${$(guestId)}`)
+    if($(this).hasClass("selectedGuest")){
+      $(".guest").removeClass("selectedGuest");
+    }
+    else if($(".selectedGuest").length){
+      $(".guest").removeClass("selectedGuest")
+      $(this).addClass("selectedGuest")
+    }else{
+      $(this).addClass("selectedGuest")
+    }
+
+  })
+  
+  guestCount++
+  Count_guest.text(guestCount);
+}
+
+
 
 // clike the room
 $(".room").on("click", function() {
   console.log(`click ${ $(this)[0].id }`)
-  if($(".selcetedGuest").length){
-    guestToRoom($(this))
+  if($(".selectedGuest").length){
+    if($(this).hasClass("active")){
+      return
+    }
+    const $selected = $(".selectedGuest");
+    const currentPos = $selected.offset();
+    $selected.appendTo("#main").css({
+        "position": "absolute",
+        "left": currentPos.left + "px",
+        "top": currentPos.top + "px",
+        "z-index": 10000
+    });
+    moveTo($(this), $selected)
+    
+    
+    
   }else{
-    moveTo($(this), $("#mainCharacter"))
+    moveTo($(this), MainCharacter)
   }
 })
+
+
 
 // clike the interact_item
 $(".interact_item").on("click", function(){
   console.log(`click ${ $(this)[0].id }`)
-  moveTo($(this),$("#mainCharacter"))
+  moveTo($(this),MainCharacter)
   //trolleyOP($(this))
 })
 
-function guestToRoom($room){
+async function moveTo($destination, $character = MainCharacter) {
   
-  if(!$room.data("active")){
-  let $guest = $(".selectedGuest")
+  if (isMoving) return;
+
+  isMoving = true;
+  $(".interact_area").css("cursor", "not-allowed");
+  $(".interact_item").css("cursor", "not-allowed");
+  
+  const currentFloor = $character.data("floor")
+  const targetFloor = $destination.data("floor")
+  
+
+  if (currentFloor !== undefined && targetFloor !== undefined && currentFloor !== targetFloor) {
+  
+    await calculateAndMove($(`#lift_${currentFloor}`), $character, speed)
+    await calculateAndMove($(`#lift_${targetFloor}`), $character, speed)
+    
+    $character.data("floor", targetFloor)
+    
+  }
+
+
+  await calculateAndMove($destination, $character, speed)
+  isMoving = false;
+  $(".interact_area").css("cursor", "default");
+  $(".interact_item").css("cursor", "default");
+  if($character.hasClass("selectedGuest")){
+    $character.css({
+      "display": "none",
+      "transition": "filter 2s ease"
+    })
+    $destination.addClass("active")
+    $character.removeClass("selectedGuest")
+  }
   
 }
-}
+
+
 function calculateAndMove($target, $character, speed = 500) {
   return new Promise((resolve) => {
-    
-    const areaOff = $target.offset();
-    const charOff = $character.offset();
+    $character.css("transition", "none");
 
-    // 1. 取得目標座標 (統一腳底對齊)
-    const targetX = areaOff.left + $target.outerWidth() / 2 - $character.outerWidth() / 2;
-    let targetY = areaOff.top + $target.outerHeight()/2 - $character.outerHeight();
+    const areaPos = $target.position();
+    const charPos = $character.position();
 
-    const deltaX = targetX - charOff.left;
-    const deltaY = targetY - charOff.top;
+    const targetX = areaPos.left + ($target.outerWidth() / 2) - ($character.outerWidth() / 2);
+    let targetY = areaPos.top + ($target.outerHeight() / 2) - $character.outerHeight()*0.6;
+
+    if ($target.hasClass("interact_item")) {
+      console.log("interact_item set y");
+      targetY = areaPos.top + ($target.outerHeight() / 2) - (1.5 * $character.outerHeight());
+    }
+
+    const deltaX = targetX - charPos.left;
+    const deltaY = targetY - charPos.top;
     const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-    
     const duration = Math.max(distance / speed, 0.1);
 
-    if($target.hasClass("interact_item")){
-      console.log("interact_item set y")
-      targetY = areaOff.top + $target.outerHeight()/2 - 2*$character.outerHeight();
-    }
     $character.css({
       left: targetX + "px",
-      top: targetY +  "px",
+      top: targetY + "px", 
       transition: `all ${duration}s linear`
     });
 
@@ -70,24 +221,7 @@ function calculateAndMove($target, $character, speed = 500) {
   });
 }
 
-async function moveTo($destination, $character = $("#mainCharacter")) {
-  const currentFloor = $character.data("floor");
-  const targetFloor = $destination.data("floor");
 
-
-  if (currentFloor !== undefined && targetFloor !== undefined && currentFloor !== targetFloor) {
-  
-    await calculateAndMove($(`#lift_${currentFloor}`), $character, speed);
-    await calculateAndMove($(`#lift_${targetFloor}`), $character, speed);
-    
-    $character.data("floor", targetFloor);
-    
-  }
-
-
-  await calculateAndMove($destination, $character, speed);
-  
-}
 
 
 
